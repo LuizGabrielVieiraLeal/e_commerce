6240744b86294bf2a2bbfae28a8ca4e19f88515a
Gabriel Vieira <luizgabrielvieiraleal@id.uff.br>
2021-08-16 11:41:59 -0300

    Iniciando a implementação do PubSub

--- apps/api/src/types/resolverTypes.ts
+++ apps/api/src/types/resolverTypes.ts
@@ -5,3 +5,25 @@ export type Resolver<
   TArgs,
   TSource = Record<string, unknown>
 > = GraphQLFieldResolver<TSource, Context, TArgs>
+
+enum MutationType {
+  CREATED = 'CREATED',
+  UPDATED = 'UPDATED',
+  DELETED = 'DELETED',
+}
+
+export interface SubscriptionArgs {
+  where: {
+    mutationIn: MutationType[]
+  }
+}
+
+export interface SubscriptionPayload<T> {
+  mutation: MutationType
+  node: T
+}
+
+export interface SubscriptionResolver<TNode, TSource = any> {
+  subscribe: Resolver<SubscriptionArgs, TSource>
+  resolve: Resolver<SubscriptionArgs, SubscriptionPayload<TNode>>
+}
